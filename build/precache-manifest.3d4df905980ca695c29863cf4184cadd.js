cdfa42dc91f82c0043ef52873d6acc040edf1618
Thierry Charbonnel <t@ap.cx>
2019-11-21 14:29:14 -0500

    prepare to deploy

---
+++ build/precache-manifest.3d4df905980ca695c29863cf4184cadd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d84010394d4115c3bc64",
+    "url": "/thierryc/react-fullpage-example/static/js/runtime~main.6dee5278.js"
+  },
+  {
+    "revision": "c7da36a9bedbcb981d8e",
+    "url": "/thierryc/react-fullpage-example/static/js/main.a4612f8d.chunk.js"
+  },
+  {
+    "revision": "780b4ff8e57a5029828e",
+    "url": "/thierryc/react-fullpage-example/static/js/2.b2d4ed16.chunk.js"
+  },
+  {
+    "revision": "c7da36a9bedbcb981d8e",
+    "url": "/thierryc/react-fullpage-example/static/css/main.fb53121b.chunk.css"
+  },
+  {
+    "revision": "9e7a7c96653eed75b686b1e63e0034d0",
+    "url": "/thierryc/react-fullpage-example/index.html"
+  }
+];
